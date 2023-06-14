9bdb05ade13cb64c1a789510c79b459960120ce6
Omci <melloulchi.omar@gmail.com>
2023-06-14 07:51:27 +0200

    Search bar implemented with name and ingredients filter based

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,45 +1,60 @@
 import { useState, useEffect } from "react";
 
 function Home() {
-  const [menu, setMenu] = useState([]);
+  const [recipes, setRecipes] = useState([]);
+  const [search, setSearch] = useState("");
+
+  const handleSearch = (e) => {
+    setSearch(e.target.value);
+  };
 
   useEffect(() => {
-    console.log("toto");
     fetch("http://localhost:8000/recipes")
       .then((res) => res.json())
       .then((data) => {
-        setMenu(data);
+        setRecipes(data);
       });
   }, []);
+
+  const filteredRecipes = recipes.filter((recipe) => {
+    const matchSearch =
+      recipe.name.toLowerCase().includes(search.toLowerCase()) ||
+      recipe.ingredients.toLowerCase().includes(search.toLowerCase());
+      return matchSearch
+  });
 
   return (
     <>
       <h1>Hubert Eats</h1>
-      <div className="">
-      <input
+      <div className="search-container">
+        <form>
+        <label htmlFor="search">Search for recipes or ingredients</label>
+        <input
           className="searchbar"
-          type="text"
-          // onChange={(e) => setSearch(e.target.value)}
-          value={""}
-          placeholder="Search your recipe"
+          type="search"
+          onChange={handleSearch}
+          value={search}
+          placeholder="Search your recipe or ingredients"
         />
+        <button type="submit">Go</button>  
+        </form>
       </div>
-      
-        
+
       <div className="recipes-container">
-      {menu.map((e, key) => {
-        return (
-          <div key={key}  className="recipes-cards">
-            <p>{e.name}</p>
-            <img src={e.img_url} alt="food"/>
-            <button type="button" className="btn-filter" onClick={""}>
-          <span>Add to your menu</span>
-        </button>
-          </div>
-        );
-      })}
+        {/* {console.log(data)} */}
+
+        {filteredRecipes.map((e, key) => {
+          return (
+            <div key={key} className="recipes-cards">
+              <p>{e.name}</p>
+              <img src={e.img_url} alt="food" />
+              <button type="button" className="btn-filter" onClick={""}>
+                <span>Add to your menu</span>
+              </button>
+            </div>
+          );
+        })}
       </div>
-      
     </>
   );
 }
